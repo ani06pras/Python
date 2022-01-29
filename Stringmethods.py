ff12c6430c1ea3dd5d61c76320fc53a112cf51ac
ani06pras <74187497+ani06pras@users.noreply.github.com>
2022-01-29 13:43:45 +0530

    Create Stringmethods.py

---
+++ Stringmethods.py
@@ -0,0 +1,16 @@
+a = "698920"
+b = "anirudh"
+
+print(a.isalpha())
+print(a.isalnum())
+print(a.isascii())
+print(a.isalpha())
+print(a.isdecimal())
+print(a.isdigit())
+print(a.isidentifier())
+print(b.islower())
+print(b.isupper())
+print(b.istitle())
+print(b.isspace())
+print(a.isnumeric())
+print(a.isprintable())
