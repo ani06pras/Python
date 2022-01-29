a0333bc11525a64f9faa978fc72d2384475f6234
ani06pras <74187497+ani06pras@users.noreply.github.com>
2022-01-29 13:41:59 +0530

    Update and rename CircleArea.py to Circle_Area_Perimeter.py

--- Circle_Area_Perimeter.py
+++ Circle_Area_Perimeter.py
@@ -2,6 +2,6 @@ print("Area of the Cirle")
 pi=3.14
 r=float(input("Enter The Radius Value:"))
 area=int(pi*r*r) 
+perimeter=int(2*pi*r)
 print("\nThe area of the circle with radius ",r," is ",area)
-print("\nRadius Value Type --> ",type(r))
-print("Area Value Type -->",type(area))
+print("\nThe Perimeter of the Circle with Radius ",r," is ",perimeter)
