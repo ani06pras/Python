f192344431221990d5a55a6f6405526995f7f92c
ani06pras <74187497+ani06pras@users.noreply.github.com>
2022-01-29 13:13:16 +0530

    Create CircleArea.py

---
+++ CircleArea.py
@@ -0,0 +1,7 @@
+print("Area of the Cirle")
+pi=3.14
+r=float(input("Enter The Radius Value:"))
+area=int(pi*r*r) 
+print("\nThe area of the circle with radius ",r," is ",area)
+print("\nRadius Value Type --> ",type(r))
+print("Area Value Type -->",type(area))
